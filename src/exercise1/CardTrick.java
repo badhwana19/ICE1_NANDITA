caaff753dc853ddc320edebb618c5588846aaa1e
badhwana19 <badhwana@sheridencollege.ca>
2023-01-28 14:50:08 -0500

    ICE 1 version 1 branch 1.2

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -25,12 +25,9 @@ public class CardTrick {
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
-            int a = (int) (Math.random() * 13) + 1;
-            card.setValue(a);
-            int b = (int) (Math.random() * 4);
-            card.setSuit(card.SUITS[b]);
-            System.out.println(a);
-            System.out.println(b);
+            
+            card.setValue((int) (Math.random() * 13) + 1);
+            card.setSuit(card.SUITS[(int) (Math.random() * 4)]);
             hand[i] = card;
         }
         
